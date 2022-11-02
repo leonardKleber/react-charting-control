166ff180d5ce483b72f856b5bd7c54425166f312
Leonard Kleber <83468990+leonardKleber@users.noreply.github.com>
2022-11-02 20:21:57 +0100

    Update ControlView.jsx

--- controller/src/components/ControlView.jsx
+++ controller/src/components/ControlView.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import Chart from './Chart.jsx';
 
-const baseURL = 'http://127.0.0.1:8000/example/';
-
-function ControlView(props) {
-  return (
-    <React.Fragment>
-      <Chart url={baseURL}/>
-    </React.Fragment>
-  )
-}
-
-export default ControlView;
