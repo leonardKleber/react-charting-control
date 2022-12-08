97b687ff5cde3e7caa48dd9914a3910ecaaee967
leonardKleber <leonard.kleber@outlook.com>
2022-12-08 17:54:54 +0100

    Add MUI5 design

--- controller/src/components/Control.jsx
+++ controller/src/components/Control.jsx
@@ -1,7 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { useEffect } from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 const baseURL = 'http://localhost:8000/chart/';
 
@@ -12,6 +21,12 @@ const baseURL = 'http://localhost:8000/chart/';
 function Control() {
   // Stores the SVG data that will be retrieved from the API.
   const [svgData, setSvgData] = useState('');
+  
+  // Stores the preset that was selected in the selector field.
+  const [preset, setPreset] = useState('');
+  const handlePresetField = (event) => {
+    setPreset(event.target.value);
+  };
 
   // Sends a post request to the API.
   function render_chart() {
@@ -27,77 +42,116 @@ function Control() {
 
   return(
     <React.Fragment>
+      {/* *************************************************************
+        This is the structure of the Chart Panel and the Zoom Buttons.
+      ************************************************************* */}
       <TransformWrapper initialScale={1} centerOnInit={true}>
-        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-          <React.Fragment>
-            <div>
-              <button 
-                onClick={() => zoomIn()}
-                style={{
-                  position: 'absolute',
-                  top: '10%',
-                  left: '75%',
-                  width: '4%',
-                  height: '4%'
-                }}
-              >
-                +
-              </button>
-              <button 
-                onClick={() => zoomOut()}
-                style={{
-                  position: 'absolute',
-                  top: '10%',
-                  left: '80%',
-                  width: '4%',
-                  height: '4%'
-                }}
-              >
-                -
-              </button>
-              <button 
-                onClick={() => resetTransform()}
-                style={{
-                  position: 'absolute',
-                  top: '10%',
-                  left: '85%',
-                  width: '4%',
-                  height: '4%'
-                }}
-              >
-                Reset
-              </button>
-            </div>
-            <TransformComponent
-              wrapperStyle={{
-                position: 'absolute',
-                top: '15%',
-                left: '15%',
-                width: '70%',
-                height: '70%',
-                //outlineColor: 'black',
-                //outlineWidth: '3px',
-                //outlineStyle: 'solid'
-              }}
-            >
-              <div id="chart" dangerouslySetInnerHTML={{__html: svgData}}/>
-            </TransformComponent>
-          </React.Fragment>
-        )}
-      </TransformWrapper>
-      <div>
-        <button 
-          onClick={render_chart}
-          style={{
+      {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+        <React.Fragment>
+          <div style={{
             position: 'absolute',
-            top: '90%',
-            left: '15%',
-            width: '70%',
-            height: '4%'
-          }}
-        >
-          Render
-        </button>
+            right: '10%'
+          }}>
+            <Stack spacing={1} direction="row">
+              <Button onClick={() => zoomIn()} variant="outlined">+</Button>
+              <Button onClick={() => zoomOut()} variant="outlined">-</Button>
+              <Button onClick={() => resetTransform()} variant="outlined">Reset</Button>
+            </Stack>
+          </div>
+          <TransformComponent
+            wrapperStyle={{
+              backgroundColor: '#EEEEEE',
+              position: 'absolute',
+              width: '80%',
+              height: '70%',
+              left: '10%',
+              top: '5%'
+            }}
+          >
+            <div id="chart" dangerouslySetInnerHTML={{__html: svgData}}/>
+          </TransformComponent>
+        </React.Fragment>
+      )}
+      </TransformWrapper>
+      {/* *************************************************************
+        This is the structure of the Settings Panel.
+      ************************************************************* */}
+      <div style={{
+        position: 'absolute',
+        top: '76%',
+        left: '10%',
+        width: '80%'
+      }}>
+        <Box sx={{flexGrow: 1}}>
+          <Grid container spacing={1}>
+            <Grid item xs>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="select-label">Preset</InputLabel>
+                  <Select
+                    labelId="select"
+                    value={preset}
+                    label="Preset"
+                    onChange={handlePresetField}
+                  >
+                    <MenuItem value={1}>One</MenuItem>
+                    <MenuItem value={2}>Two</MenuItem>
+                    <MenuItem value={3}>Three</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <FormControl fullWidth>
+                <TextField label='Width' variant='outlined'/>
+              </FormControl>
+            </Grid>
+            <Grid item xs>
+              <FormControl fullWidth>
+                <TextField label='Height' variant='outlined'/>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+      {/* *************************************************************
+        This is the structure of the Download Panel.
+      ************************************************************* */}
+      <div style={{
+        position: 'absolute',
+        top: '84%',
+        left: '10%',
+        width: '80%',
+      }}>
+        <Box sx={{flexGrow: 1}}>
+          <Grid container spacing={1}>
+            <Grid item xs>
+              <FormControl fullWidth>
+                <TextField label='Filename' variant='outlined'/>
+              </FormControl>
+            </Grid>
+            <Grid item xs>
+              <FormControl fullWidth>
+                <Button variant="outlined">Download</Button>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+      {/* *************************************************************
+        This is the structure of the Render Button.
+      ************************************************************* */}
+      <div style={{
+        position: 'absolute',
+        top: '92%',
+        left: '10%',
+        width: '80%'
+      }}>
+        <Box sx={{flexGrow: 1}}>
+          <FormControl fullWidth>
+            <Button onClick={() => render_chart()} variant="contained">Render</Button>
+          </FormControl>
+        </Box>
       </div>
     </React.Fragment>
   )
